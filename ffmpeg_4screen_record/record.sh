24e0e0b605c614f9d2223d44be54268bd08e05e6
stSLAYER <85291546+stSLAYER@users.noreply.github.com>
2021-08-05 19:10:22 +0200

    Update record.sh

--- ffmpeg_4screen_record/record.sh
+++ ffmpeg_4screen_record/record.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:10 /home/imraan/Desktop/ffmpeg_4screen_record/screen_0.mp4
-# /home/imraan/Desktop/ffmpeg_4screen_record
+# ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:10 /home/$USER/Desktop/ffmpeg_4screen_record/screen_0.mp4
+# /home/$USER/Desktop/ffmpeg_4screen_record
 
 # This will stream and save every segmented second:
 # ffmpeg -rtsp_transport tcp -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov -c copy -f segment -segment_time 5 stream_cam1_%d.mp4
@@ -22,11 +22,11 @@
 while :
 do
 	# creates main dir and sub dir
-	mkdir -p /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams
-	mkdir -p /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams/camera_1
-	mkdir -p /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams/camera_2
-	mkdir -p /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams/camera_3
-	mkdir -p /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams/camera_4
+	mkdir -p /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams
+	mkdir -p /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams/camera_1
+	mkdir -p /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams/camera_2
+	mkdir -p /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams/camera_3
+	mkdir -p /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams/camera_4
 	
 	#ffmpeg stream record camera 1
 	gnome-terminal --tab --title="cam1" --command="ffmpeg -rtsp_transport tcp -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov -c copy -f segment -segment_time 10 cam_1_%d.mp4"
@@ -42,7 +42,7 @@ do
 
 	
 	#moves the cam_1_*.mp4 file to camer_streams/camera_1
-	mv /home/imraan/Desktop/ffmpeg_4screen_record/cam_1_*.mp4 /home/imraan/Desktop/ffmpeg_4screen_record/camera_streams/camera_1
+	mv /home/$USER/Desktop/ffmpeg_4screen_record/cam_1_*.mp4 /home/$USER/Desktop/ffmpeg_4screen_record/camera_streams/camera_1
 
 done
 

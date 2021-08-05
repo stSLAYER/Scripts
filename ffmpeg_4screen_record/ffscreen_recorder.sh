5bff18ac0b64c6f96eef276129275d28071c68fa
stSLAYER <85291546+stSLAYER@users.noreply.github.com>
2021-08-05 19:11:36 +0200

    Update ffscreen_recorder.sh

--- ffmpeg_4screen_record/ffscreen_recorder.sh
+++ ffmpeg_4screen_record/ffscreen_recorder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:10 /home/imraan/Desktop/ffmpeg_4screen_record/screen_0.mp4
-# /home/imraan/Desktop/ffmpeg_4screen_record
+# ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:10 /home/$USER/Desktop/ffmpeg_4screen_record/screen_0.mp4
+# /home/$USER/Desktop/ffmpeg_4screen_record
 
 
 #############################################################################################
@@ -18,12 +18,12 @@ do
 	#checks to see if archive dir is greater than a certain size in MB, if it is, it gets moved to cloud_storage
 	ARCHIVE_TIME=`date +%F+%T | nawk '{gsub(/:/,"-")};1' | nawk '{gsub(/+/,"-")};1'`
 	if [ $(du -m -s archive/ | cut -f1) -gt 4 ] ; then 
-  		mv archive/ /home/imraan/Desktop/cloud_storage/archive_$ARCHIVE_TIME
+  		mv archive/ /home/$USER/Desktop/cloud_storage/archive_$ARCHIVE_TIME
 	fi
 	
 	#zip the archive file and adds a date to it
 	#ARCHIVE_TIME=`date +%F+%T | nawk '{gsub(/:/,"-")};1' | nawk '{gsub(/+/,"-")};1'`
-	#cd /home/imraan/Desktop/cloud_storage/ && zip -r /home/imraan/Desktop/cloud_storage/archive_$ARCHIVE_TIME.zip archive  && rm -r /home/imraan/Desktop/cloud_storage/archive && cd /home/imraan/Desktop/ffmpeg_4screen_record
+	#cd /home/$USER/Desktop/cloud_storage/ && zip -r /home/$USER/Desktop/cloud_storage/archive_$ARCHIVE_TIME.zip archive  && rm -r /home/$USER/Desktop/cloud_storage/archive && cd /home/$USER/Desktop/ffmpeg_4screen_record
 
 
 	#HIDE_TERM=`xdotool search --onlyvisible --class terminal | head -1 | tail -1`
@@ -31,11 +31,11 @@ do
 	
 	#moves it to the archive/ dir
 	CURRENT_TIME=`date +%F+%T | nawk '{gsub(/-/,"")};1' | nawk '{gsub(/:/,"-")};1' | nawk '{gsub(/+/,"-")};1'`
-	mv /home/imraan/Desktop/ffmpeg_4screen_record/*.mp4 /home/imraan/Desktop/ffmpeg_4screen_record/archive/
+	mv /home/$USER/Desktop/ffmpeg_4screen_record/*.mp4 /home/$USER/Desktop/ffmpeg_4screen_record/archive/
 	sleep 1
 	
 	#adds the date to the screen_*.mp4 file 
-	ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:05 /home/imraan/Desktop/ffmpeg_4screen_record/screen_$CURRENT_TIME.mp4 
+	ffmpeg -f x11grab -s 1920x1080 -r 25 -i :0.0+1366,0 -t 00:00:05 /home/$USER/Desktop/ffmpeg_4screen_record/screen_$CURRENT_TIME.mp4 
 	sleep 2
 
 done
